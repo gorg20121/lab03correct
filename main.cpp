68b454ac0dcb975ab7103e7b0c3bd540049b7159
kapkinev <example@mail.ru>
2020-05-28 23:55:39 +0300

    code: Аргументы программы

--- main.cpp
+++ main.cpp
@@ -39,8 +39,16 @@ return data;
 }
 
 
-int main()
-{
+
+
+int main(int argc, char* argv[]) {
+
+    if (argc > 1)
+    {
+        cerr<< argc;
+        for (int i=0; i<argc ; i++)
+            cerr<< argv[i];
+        return 0;}
     curl_global_init(CURL_GLOBAL_ALL);
     const auto input = read_input(cin,true);
     const auto bins = make_histogram(input);
