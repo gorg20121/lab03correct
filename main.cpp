99953d39fe96a6149c0f19bcbc58df902e04a87b
kapkinev <example@mail.ru>
2020-05-29 00:03:15 +0300

    code:сохранение данных в буфер

--- main.cpp
+++ main.cpp
@@ -3,29 +3,34 @@
 #include <curl/curl.h>
 #include "histogram.h"
 #include "svg.h"
+#include <sstream>
+#include <string>
 using namespace std;
 
 
 
-vector<double> input_numbers(istream& in, size_t count) {
+vector<double> input_numbers(istream& in, size_t count)
+{
     vector<double> result(count);
-    for (size_t i = 0; i < count; i++) {
+    for (size_t i = 0; i < count; i++)
+    {
         in >> result[i];
     }
     return result;
 }
 
 
-Input read_input(istream& in, bool prompt) {
+Input read_input(istream& in, bool prompt)
+{
     Input data;
     size_t number_count;
-     if (prompt)
-     {
-         cerr << "Enter number count: ";
-    in >> number_count;
-    cerr << "Enter numbers: ";
-    data.numbers = input_numbers(in, number_count);
-    cerr << "Enter bin count : ";
+    if (prompt)
+    {
+        cerr << "Enter number count: ";
+        in >> number_count;
+        cerr << "Enter numbers: ";
+        data.numbers = input_numbers(in, number_count);
+        cerr << "Enter bin count : ";
         in >> data.bin_count;
     }
     else
@@ -34,34 +39,54 @@ Input read_input(istream& in, bool prompt) {
         data.numbers = input_numbers(in, number_count);
         in >> data.bin_count;
 
-}
-return data;
-}
-
-
-
-
-int main(int argc, char* argv[]) {
-
-    if (argc > 1)
-    {
-        CURL *curl = curl_easy_init();
-        if(curl) {
-            CURLcode res;
-            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
-            res = curl_easy_perform(curl);
-            if (res != CURLE_OK ){
-                 cerr<<curl_easy_strerror(res);
-                 exit(1);
-            }
-            curl_easy_cleanup(curl);
-        }
-        return 0;
     }
+    return data;
+}
+size_t write_data(void* items, size_t item_size, size_t item_count, void* ctx)
+{
+    const size_t data_size = item_size * item_count;
+    const char* new_items = reinterpret_cast<const char*>(items);
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    buffer->write(new_items, data_size);
+    return data_size;
+}
 
+
+
+
+Input download(const string& address)
+{
+    stringstream buffer;
     curl_global_init(CURL_GLOBAL_ALL);
-    const auto input = read_input(cin,true);
-    const auto bins = make_histogram(input);
+
+
+    CURL *curl = curl_easy_init();
+    if(curl)
+    {
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL,address.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK )
+        {
+            cerr<<curl_easy_strerror(res);
+            exit(1);
+        }
+        curl_easy_cleanup(curl);
+
+    }
+    return read_input(buffer, false);
+}
+int main(int argc, char* argv[])
+{
+    Input input;
+    if (argc > 1)
+        input = download(argv[1]);
+    else
+    input = read_input(cin, true);
+
+                const auto bins = make_histogram(input);
     show_histogram_svg(bins);
     return 0;
 
