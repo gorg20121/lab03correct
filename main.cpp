f8a7bb144fe451e6f0fe4fe2304125c20de8a165
kapkinev <example@mail.ru>
2020-05-28 22:23:58 +0300

    code: main.cpp input

--- main.cpp
+++ main.cpp
@@ -4,25 +4,57 @@
 #include "svg.h"
 using namespace std;
 
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+};
+
 vector<double> input_numbers(istream& in, size_t count) {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++) {
-        cin >> result[i];
+        in >> result[i];
     }
     return result;
+}
+vector<size_t> make_histogram (size_t count,const vector<double>& numbers)
+{
+    double min=0;
+    double max=0;
+    find_minmax(numbers,min,max);
+    vector<size_t> bins(count);
+    for (double number : numbers)
+    {
+        size_t bin;
+        bin = (number - min) / (max - min) * count;
+        if (bin == count)
+        {
+            bin--;
+        }
+        bins[bin]++;
+    }
+    return bins;
+}
+
+Input read_input(istream& in) {
+    Input data;
+
+    cerr << "Enter number count: ";
+    size_t number_count;
+    in >> number_count;
+
+    cerr << "Enter numbers: ";
+    data.numbers = input_numbers(in, number_count);
+
+    cerr<< "Enter bin count: ";
+    in>> data.bin_count;
+
+    return data;
 }
 
 int main()
 {
-    size_t number_count;
-    cerr<< "enter number count";
-    cin>>number_count;
-    const auto numbers=input_numbers(cin, number_count);
-
-    size_t bin_count;
-    cerr<< "enter bin count";
-    cin>>bin_count;
-    const auto bins = make_histogram(numbers, bin_count);
+    Input data = read_input(cin);
+    const auto bins = make_histogram(data.bin_count, data.numbers);
     show_histogram_svg(bins);
     return 0;
 
